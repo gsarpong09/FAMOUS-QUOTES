2be63c0daa75450a82cf37db7e8b8b059ad3aeb5
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:48:22 -0500

    Update update.php

--- api/categories/update.php
+++ api/categories/update.php
@@ -1,2 +1,23 @@
 <?php
-// update.php for categories
+require_once '../../config/Database.php';
+require_once '../../models/Category.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->id) || !isset($data->category)) {
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit;
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Category($db);
+$category->id = $data->id;
+$category->category = $data->category;
+
+if ($category->update()) {
+    echo json_encode(['id' => $category->id, 'category' => $category->category]);
+} else {
+    echo json_encode(['message' => 'category_id Not Found']);
+}

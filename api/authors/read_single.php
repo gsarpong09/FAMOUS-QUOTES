9ec38f988f46666dc91d264cbbc0cb9ae2e76fc0
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 03:02:32 -0500

    Update read_single.php

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -1,2 +1,12 @@
 <?php
-// read_single.php for authors
+require_once '../../config/Database.php';
+require_once '../../models/Author.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$author = new Author($db);
+$author->id = $_GET['id'];
+
+$result = $author->read_single()->fetch(PDO::FETCH_ASSOC);
+echo $result ? json_encode($result) : json_encode(['message' => 'author_id Not Found']);

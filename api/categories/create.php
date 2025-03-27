65c5b5161773607d62c1ab525c9ca85e916d06cb
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:45:07 -0500

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,2 +1,22 @@
 <?php
-// create.php for categories
+require_once '../../config/Database.php';
+require_once '../../models/Author.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->author)) {
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit;
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$author = new Author($db);
+$author->author = $data->author;
+
+if ($author->create()) {
+    echo json_encode(['id' => $db->lastInsertId(), 'author' => $author->author]);
+} else {
+    echo json_encode(['message' => 'Author Not Created']);
+}

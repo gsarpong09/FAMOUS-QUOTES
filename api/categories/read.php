b863d5f35d27b7738d1a38e90012d235d90b824c
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:48:57 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -1,2 +1,16 @@
 <?php
-// read.php for categories
+require_once '../../config/Database.php';
+require_once '../../models/Category.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Category($db);
+$result = $category->read();
+
+$categories = [];
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
+    $categories[] = ['id' => $id, 'category' => $category];
+}
+echo json_encode($categories);

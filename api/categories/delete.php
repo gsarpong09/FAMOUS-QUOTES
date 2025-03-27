c9922f3075f4b7396985569da930b80b12fd76aa
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:45:24 -0500

    Update delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -1,2 +1,22 @@
 <?php
-// delete.php for categories
+require_once '../../config/Database.php';
+require_once '../../models/Author.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->id)) {
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit;
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$author = new Author($db);
+$author->id = $data->id;
+
+if ($author->delete()) {
+    echo json_encode(['id' => $author->id]);
+} else {
+    echo json_encode(['message' => 'author_id Not Found']);
+}

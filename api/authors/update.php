dd1ae04835483d62c6626e0a48a7559d1b64719b
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:46:47 -0500

    Update update.php

--- api/authors/update.php
+++ api/authors/update.php
@@ -1,2 +1,23 @@
 <?php
-// update.php for authors
+require_once '../../config/Database.php';
+require_once '../../models/Author.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->id) || !isset($data->author)) {
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit;
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$author = new Author($db);
+$author->id = $data->id;
+$author->author = $data->author;
+
+if ($author->update()) {
+    echo json_encode(['id' => $author->id, 'author' => $author->author]);
+} else {
+    echo json_encode(['message' => 'author_id Not Found']);
+}

4e6647e5888bac04c1d94c5fe24b19aa00aa82ba
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:50:16 -0500

    Update create.php

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -1,2 +1,24 @@
 <?php
-// create.php for quotes
+require_once '../../config/Database.php';
+require_once '../../models/Quote.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->quote) || !isset($data->author_id) || !isset($data->category_id)) {
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit;
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$quote = new Quote($db);
+$quote->quote = $data->quote;
+$quote->author_id = $data->author_id;
+$quote->category_id = $data->category_id;
+
+if ($quote->create()) {
+    echo json_encode(['message' => 'Quote Created']);
+} else {
+    echo json_encode(['message' => 'Quote Not Created']);
+}

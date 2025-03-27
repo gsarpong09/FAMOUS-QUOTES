266fa546f8beabcc0fb25627f02f889c98fc3518
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:51:23 -0500

    Update read_single.php

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -1,2 +1,12 @@
 <?php
-// read_single.php for quotes
+require_once '../../config/Database.php';
+require_once '../../models/Quote.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$quote = new Quote($db);
+$quote->id = $_GET['id'];
+
+$result = $quote->read_single()->fetch(PDO::FETCH_ASSOC);
+echo $result ? json_encode($result) : json_encode(['message' => 'No Quotes Found']);

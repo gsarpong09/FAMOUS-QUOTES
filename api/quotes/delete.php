86a970519925023ac094acefb5cc5a19a7ac9c0c
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:50:39 -0500

    Update delete.php

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -1,2 +1,22 @@
 <?php
-// delete.php for quotes
+require_once '../../config/Database.php';
+require_once '../../models/Quote.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->id)) {
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit;
+}
+
+$database = new Database();
+$db = $database->connect();
+
+$quote = new Quote($db);
+$quote->id = $data->id;
+
+if ($quote->delete()) {
+    echo json_encode(['message' => 'Quote Deleted']);
+} else {
+    echo json_encode(['message' => 'No Quotes Found']);
+}

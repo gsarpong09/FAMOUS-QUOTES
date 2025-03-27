73ac7df154227788591c5fb2c26f53fd8117ddf8
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-27 02:51:05 -0500

    Update read.php

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,2 +1,16 @@
 <?php
-// read.php for quotes
+require_once '../../config/Database.php';
+require_once '../../models/Quote.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$quote = new Quote($db);
+$result = $quote->read();
+
+$quotes = [];
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
+    $quotes[] = ['id' => $id, 'quote' => $quote, 'author' => $author, 'category' => $category];
+}
+echo json_encode($quotes);

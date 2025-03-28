c1f580b50959f232b112800f702982bd3ffd126d
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 15:58:48 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -2,34 +2,37 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-require_once '../../config/Database.php';
-require_once '../../models/Category.php';
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
 $database = new Database();
 $db = $database->connect();
 
 $category = new Category($db);
-$result = $category->read();
 
-if ($result) {
-    if ($result->rowCount() > 0) {
-        $data = [];
-
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            extract($row);
-            $data[] = [
-                'id' => $id,
-                'category' => $category
-            ];
-        }
-
-        http_response_code(200);
-        echo json_encode($data);
-    } else {
-        http_response_code(404);
-        echo json_encode(['message' => 'No categories found.']);  
-    }
+if (isset($_GET['id'])) {
+    $category->id = $_GET['id'];
+    $result = $category->read_single();
 } else {
-    http_response_code(500);
-    echo json_encode(['message' => 'Failed to query database.']);
+    $result = $category->read();
 }
+
+$num = $result->rowCount();
+
+if ($num > 0) {
+    $categories_arr = [];
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $categories_arr[] = [
+            'id' => $id,
+            'category' => $category
+        ];
+    }
+
+    echo json_encode($categories_arr);
+} else {
+    echo json_encode(['message' => 'No Categories Found']);
+}
+?>

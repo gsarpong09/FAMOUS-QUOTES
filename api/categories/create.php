508ff7b86fb2653452e8fbb99a709c591f52db93
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 16:44:03 -0500

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,25 +1,38 @@
+<?php
+// Set response headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-<?php
-require_once '../../config/Database.php';
-require_once '../../models/Author.php';
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate category object
+$category = new Category($db);
+
+// Get raw input data
 $data = json_decode(file_get_contents("php://input"));
 
-if (!isset($data->author)) {
+// Validate input
+if (!isset($data->category)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
     exit;
 }
 
-$database = new Database();
-$db = $database->connect();
+// Assign to object
+$category->category = $data->category;
 
-$author = new Author($db);
-$author->author = $data->author;
-
-if ($author->create()) {
-    echo json_encode(['id' => $db->lastInsertId(), 'author' => $author->author]);
+// Attempt to create
+if ($category->create()) {
+    echo json_encode([
+        'id' => $category->id,
+        'category' => $category->category
+    ]);
 } else {
-    echo json_encode(['message' => 'Author Not Created']);
+    echo json_encode(['message' => 'Category Not Created']);
 }

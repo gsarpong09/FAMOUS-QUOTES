af34fd3e101ffe6f234d7f9d502b147dc332a032
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 15:46:50 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -11,10 +11,25 @@ $db = $database->connect();
 $category = new Category($db);
 $result = $category->read();
 
-$categories = [];
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    extract($row);
-    $categories[] = ['id' => $id, 'category' => $category];
-}
+if ($result) {
+    if ($result->rowCount() > 0) {
+        $data = [];
 
-echo json_encode($categories);
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $data[] = [
+                'id' => $id,
+                'category' => $category
+            ];
+        }
+
+        http_response_code(200);
+        echo json_encode($data);
+    } else {
+        http_response_code(404);
+        echo json_encode(['message' => 'No categories found.']);  
+    }
+} else {
+    http_response_code(500);
+    echo json_encode(['message' => 'Failed to query database.']);
+}

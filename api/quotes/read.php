9b7281a28ecb04b1ebc3c5b0119948df6d52a4c8
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 15:14:21 -0500

    Update read.php

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,25 +1,32 @@
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-<?php
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
 
+// Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
+
 
 $quote = new Quote($db);
 
 
-$author_id = $_GET['author_id'] ?? null;
-$category_id = $_GET['category_id'] ?? null;
+if (isset($_GET['id'])) {
+    $result = $quote->read_single($_GET['id']);
+} elseif (isset($_GET['author_id']) && isset($_GET['category_id'])) {
+    $result = $quote->read_by_author_and_category($_GET['author_id'], $_GET['category_id']);
+} elseif (isset($_GET['author_id'])) {
+    $result = $quote->read_by_author($_GET['author_id']);
+} elseif (isset($_GET['category_id'])) {
+    $result = $quote->read_by_category($_GET['category_id']);
+} else {
+    $result = $quote->read();
+}
 
-$result = $quote->read($author_id, $category_id);
-$num = $result->rowCount();
-
-if ($num > 0) {
+if ($result && $result->rowCount() > 0) {
     $quotes_arr = [];
-
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
         $quotes_arr[] = [
@@ -29,7 +36,6 @@ if ($num > 0) {
             'category' => $category
         ];
     }
-
     echo json_encode($quotes_arr);
 } else {
     echo json_encode(['message' => 'No Quotes Found']);

28f10014b02d74961073f722cee94f8cb35731e0
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 16:43:23 -0500

    Update delete.php

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -1,25 +1,35 @@
+<?php
+// Set response headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-<?php
-require_once '../../config/Database.php';
-require_once '../../models/Author.php';
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
 
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate author object
+$author = new Author($db);
+
+// Get raw input data
 $data = json_decode(file_get_contents("php://input"));
 
+// Validate input
 if (!isset($data->id)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
     exit;
 }
 
-$database = new Database();
-$db = $database->connect();
-
-$author = new Author($db);
+// Set ID to delete
 $author->id = $data->id;
 
+// Attempt to delete
 if ($author->delete()) {
     echo json_encode(['id' => $author->id]);
 } else {
-    echo json_encode(['message' => 'author_id Not Found']);
+    echo json_encode(['message' => 'Author Not Deleted']);
 }

e614143917e3a2dd4b24b982aadcadc9e23b7597
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 00:52:12 -0500

    Update index.php

--- api/categories/index.php
+++ api/categories/index.php
@@ -1,13 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+
 $method = $_SERVER['REQUEST_METHOD'];
+
 if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
 }
 
+// Main routing switch
 switch ($method) {
     case 'GET':
         if (isset($_GET['id'])) {
@@ -24,5 +31,8 @@ switch ($method) {
         break;
     case 'DELETE':
         require 'delete.php';
+        break;
+    default:
+        echo json_encode(['message' => 'Invalid request method']);
         break;
 }

81d6864d4c91509e18cd3af505daaffc4785c5bb
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 00:30:40 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
 require_once '../../config/Database.php';
 require_once '../../models/Category.php';
 
@@ -13,4 +16,5 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     extract($row);
     $categories[] = ['id' => $id, 'category' => $category];
 }
+
 echo json_encode($categories);

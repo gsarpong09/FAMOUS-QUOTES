bac34ce8f887624865582e32ea6370eb3a00d90d
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 02:20:16 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,21 +1,9 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    exit(0);
-}
-
-
 echo json_encode([
-    "message" => "Welcome to the Famous Quotes API!",
-    "routes" => [
-        "/quotes/" => "Access all quote endpoints",
-        "/authors/" => "Access all author endpoints",
-        "/categories/" => "Access all category endpoints"
+    'message' => 'Welcome to the Famous Quotes API!',
+    'routes' => [
+        '/quotes/' => 'Access all quote endpoints',
+        '/authors/' => 'Access all author endpoints',
+        '/categories/' => 'Access all category endpoints'
     ]
 ]);

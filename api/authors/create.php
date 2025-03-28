779668ee5efad19c03e1e383240d2009ec675f79
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 16:42:16 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -1,25 +1,37 @@
+<?php
+// Set response headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-<?php
-require_once '../../config/Database.php';
-require_once '../../models/Author.php';
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
 
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate author object
+$author = new Author($db);
+
+// Get raw posted data
 $data = json_decode(file_get_contents("php://input"));
 
+// Validate input
 if (!isset($data->author)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
     exit;
 }
 
-$database = new Database();
-$db = $database->connect();
-
-$author = new Author($db);
 $author->author = $data->author;
 
+// Attempt to create author
 if ($author->create()) {
-    echo json_encode(['id' => $db->lastInsertId(), 'author' => $author->author]);
+    echo json_encode([
+        'id' => $author->id,
+        'author' => $author->author
+    ]);
 } else {
     echo json_encode(['message' => 'Author Not Created']);
 }

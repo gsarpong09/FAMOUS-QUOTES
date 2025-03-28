3124b5a2d99d399c6342afb92a613166c4d8f9f7
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 16:41:05 -0500

    Update delete.php

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -1,25 +1,34 @@
+<?php
+// Set response headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-<?php
-require_once '../../config/Database.php';
-require_once '../../models/Quote.php';
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate quote object
+$quote = new Quote($db);
+
+// Get raw posted data
 $data = json_decode(file_get_contents("php://input"));
 
+// Validate input
 if (!isset($data->id)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
     exit;
 }
 
-$database = new Database();
-$db = $database->connect();
-
-$quote = new Quote($db);
 $quote->id = $data->id;
 
+// Attempt delete
 if ($quote->delete()) {
-    echo json_encode(['message' => 'Quote Deleted']);
+    echo json_encode(['id' => $quote->id]);
 } else {
     echo json_encode(['message' => 'No Quotes Found']);
 }

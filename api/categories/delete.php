905db8afa20fb8700c4984791569e0827002510d
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 16:44:57 -0500

    Update delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -1,25 +1,35 @@
+<?php
+// Set response headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-<?php
-require_once '../../config/Database.php';
-require_once '../../models/Author.php';
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate category object
+$category = new Category($db);
+
+// Get raw input data
 $data = json_decode(file_get_contents("php://input"));
 
+// Validate input
 if (!isset($data->id)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
     exit;
 }
 
-$database = new Database();
-$db = $database->connect();
+// Assign ID
+$category->id = $data->id;
 
-$author = new Author($db);
-$author->id = $data->id;
-
-if ($author->delete()) {
-    echo json_encode(['id' => $author->id]);
+// Attempt to delete
+if ($category->delete()) {
+    echo json_encode(['id' => $category->id]);
 } else {
-    echo json_encode(['message' => 'author_id Not Found']);
+    echo json_encode(['message' => 'Category Not Deleted']);
 }

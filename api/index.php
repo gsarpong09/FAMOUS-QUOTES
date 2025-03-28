c48a59b35580549d18c5201ab967b1caaaec956e
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 02:13:40 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,14 +1,15 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-$method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === 'OPTIONS') {
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    exit();
+    exit(0);
 }
+
 
 echo json_encode([
     "message" => "Welcome to the Famous Quotes API!",

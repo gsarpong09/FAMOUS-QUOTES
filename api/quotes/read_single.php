6296ef1cb7a7536ae14b6bdede4dccab8a3bd44f
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 16:32:10 -0500

    Update read_single.php

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -1,15 +1,34 @@
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-<?php
-require_once '../../config/Database.php';
-require_once '../../models/Quote.php';
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
 $database = new Database();
 $db = $database->connect();
 
 $quote = new Quote($db);
-$quote->id = $_GET['id'];
 
-$result = $quote->read_single()->fetch(PDO::FETCH_ASSOC);
-echo $result ? json_encode($result) : json_encode(['message' => 'No Quotes Found']);
+// Check for ID parameter
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    http_response_code(400);
+    echo json_encode(['message' => 'Missing Required Parameters']);
+    exit();
+}
+
+$quote->id = $_GET['id'];
+$quote->read_single();
+
+if ($quote->quote !== null) {
+    echo json_encode([
+        'id' => $quote->id,
+        'quote' => $quote->quote,
+        'author' => $quote->author,
+        'category' => $quote->category
+    ]);
+} else {
+    http_response_code(404);
+    echo json_encode(['message' => 'No Quotes Found']);
+}
+?>

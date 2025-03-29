e486a7b6214e3e5fbeefc261572d13bdbb1627e6
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 22:28:28 -0500

    Update read_single.php

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -1,32 +1,32 @@
 <?php
+// Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+// Include DB and Author model
 include_once '../../config/Database.php';
 include_once '../../models/Author.php';
 
+// Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
 
+// Instantiate Author object
 $author = new Author($db);
 
-// Check for ID parameter
-if (!isset($_GET['id']) || empty($_GET['id'])) {
-    http_response_code(400);
-    echo json_encode(['message' => 'Missing Required Parameters']);
+// Validate and get id
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    echo json_encode(['message' => 'Missing or invalid id parameter']);
     exit();
 }
 
+// Set ID and get data
 $author->id = $_GET['id'];
-$author->read_single();
+$result = $author->read_single();
 
-if ($author->author !== null) {
-    echo json_encode([
-        'id' => $author->id,
-        'author' => $author->author
-    ]);
+if ($result) {
+    echo json_encode($result);
 } else {
-    http_response_code(404);
     echo json_encode(['message' => 'author_id Not Found']);
 }
 ?>

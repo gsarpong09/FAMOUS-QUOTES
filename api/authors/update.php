4d93fb66cd8a130c8a5a2927ed7f31a9dced8e84
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 22:10:09 -0500

    Update update.php

--- api/authors/update.php
+++ api/authors/update.php
@@ -1,10 +1,11 @@
 <?php
-// Set response headers
+// Headers
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Methods: PUT');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
+// Include database and model
 include_once '../../config/Database.php';
 include_once '../../models/Author.php';
 
@@ -12,28 +13,29 @@ include_once '../../models/Author.php';
 $database = new Database();
 $db = $database->connect();
 
-// Instantiate author object
+// Instantiate Author object
 $author = new Author($db);
 
-// Get raw input data
+// Get raw PUT data
 $data = json_decode(file_get_contents("php://input"));
 
-// Validate input
+// Validate required fields
 if (!isset($data->id) || !isset($data->author)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
-    exit;
+    exit();
 }
 
-// Set properties
+// Set ID and new author name
 $author->id = $data->id;
 $author->author = $data->author;
 
-// Attempt to update author
+// Attempt update
 if ($author->update()) {
     echo json_encode([
         'id' => $author->id,
         'author' => $author->author
     ]);
 } else {
-    echo json_encode(['message' => 'Author Not Updated']);
+    echo json_encode(['message' => 'author_id Not Found']);
 }
+?>

377aee250113447cfaf12e1e389cf4c84ff08826
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-03-28 22:13:39 -0500

    Update delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -1,10 +1,11 @@
 <?php
-// Set response headers
+// Headers
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
+// Include DB and model
 include_once '../../config/Database.php';
 include_once '../../models/Category.php';
 
@@ -18,18 +19,19 @@ $category = new Category($db);
 // Get raw input data
 $data = json_decode(file_get_contents("php://input"));
 
-// Validate input
-if (!isset($data->id)) {
+// Check if id is provided
+if (!isset($data->id) || empty($data->id)) {
     echo json_encode(['message' => 'Missing Required Parameters']);
-    exit;
+    exit();
 }
 
-// Assign ID
+// Set ID to delete
 $category->id = $data->id;
 
-// Attempt to delete
+// Attempt delete
 if ($category->delete()) {
     echo json_encode(['id' => $category->id]);
 } else {
     echo json_encode(['message' => 'Category Not Deleted']);
 }
+?>
